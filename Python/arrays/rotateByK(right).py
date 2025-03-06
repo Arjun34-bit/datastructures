d0fb8fc1077826b0828dbc8406ab53b82e27e720
Arjun Devendra <arjundevendra34@gmail.com>
2025-03-06 20:50:39 +0530

    roateByK(Clockwise)

---
+++ Python/arrays/rotateByK(right).py
@@ -0,0 +1,21 @@
+class Rotate:
+    def __init__(self,arr):
+        self.arr=arr
+    def reverse(self,l,r):
+        while l<r:
+            self.arr[l],self.arr[r]=self.arr[r],self.arr[l]
+            l=l+1
+            r=r-1
+    def rotate(self):
+        l=0
+        n=len(arr)
+        temp=0
+        self.reverse(0,n-1)
+        self.reverse(0,0)
+        self.reverse(1,n-1)
+        return self.arr
+    
+
+arr=[1,2,3,4,5]
+r=Rotate([1,2,3,4,5])
+print(r.rotate())
