97e509203c7dc808d62d36803d1d5dd407037e5b
Arjun Devendra <arjundevendra34@gmail.com>
2025-03-07 21:39:14 +0530

    countFreq(Map)

---
+++ Python/arrays/countFreq.py
@@ -0,0 +1,15 @@
+def countFreq(a):
+    mp={}
+    for i in range(0,len(a)):
+        mp[a[i]]=mp.get(a[i],0)+1
+        
+    result=[]
+    for i in range(1,len(a)+1):
+        if i not in mp:
+            result.append(0)
+        else:
+            result.append(mp[i])
+    
+    return result
+
+print(countFreq([2,3,2,3,5]))
