55a358908ed2233ccfb7bee33bf6c57bd5740a5c
Arjun Devendra <arjundevendra34@gmail.com>
2025-03-11 20:43:13 +0530

    twoSum Better Approach

--- Python/arrays/twoSum.py
+++ Python/arrays/twoSum.py
@@ -1,3 +1,27 @@
+
+# Better Approach
+# Time Complexity is O(N)
+# Space Complexity is O(N)
+def twoSum(arr,target):
+    if(len(arr)<=1):
+        return False
+    
+    hash_map={}
+    for i in range(0,len(arr)):
+        rem=target-arr[i]
+        if rem in hash_map:
+            return True
+        else:
+            hash_map[arr[i]]=arr[i]
+    return False
+    
+    
+arr=[1,2,3,4,5,6]
+print(twoSum(arr,11))
+
+
+
+
 def twoPointer(nums,target):
     left=0
     right=len(nums)-1
@@ -15,4 +39,4 @@ def twoPointer(nums,target):
             right-=1
     return result
 
-print(twoPointer([1,2,3,4,5,6,7,8,9],10))
+# print(twoPointer([1,2,3,4,5,6,7,8,9],10))
