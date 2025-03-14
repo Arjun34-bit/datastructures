2aa97ec22fd0332fb43c8c578a5da5b0f6c713d0
Arjun Devendra <arjundevendra34@gmail.com>
2025-03-15 00:56:00 +0530

    findUnion Solved

--- Python/arrays/union.py
+++ Python/arrays/union.py
@@ -1,21 +1,27 @@
 def findUnion(arr1,arr2):
-    result={}
+    result=set()
     i=0
     j=0
-    n1=len(arr1)-1
-    n2=len(arr2)-1
+    n1=len(arr1)
+    n2=len(arr2)
     
     while i<n1 and j<n2:
         if arr1[i]<=arr2[j]:
             if len(result)==0 or arr1[i] not in result:
-                result[i]=arr1[i]
-                # print(result)
+                result.add(arr1[i])
             i+=1
         else:
-            if len(result)==0 or arr2[j] not in result:
-                result['i']=arr2[j]
-                print(result)
+            if len(result)==0 or arr2[j] not in result :
+                result.add(arr2[j])
             j+=1
+    while j<n2:
+        if len(result)==0 or arr2[j] not in result:
+            result.add(arr2[j])
+        j+=1
+    while i<n1:
+        if len(result)==0 or arr1[i] not in result:
+            result.add(arr1[i])
+        i+=1
     return len(result)
 
 
