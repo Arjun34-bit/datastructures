889274a630043a73352196c815b757d5e5234fc2
Arjun Devendra <arjundevendra34@gmail.com>
2025-03-15 14:38:06 +0530

    Time and Space Complexity for union

--- Python/arrays/union.py
+++ Python/arrays/union.py
@@ -1,26 +1,36 @@
+
+# Time Complexity is 
+# O(NLOGN) + O(NLOGN) + O(N1 +N2)= O(NLOGN)
+
+# Space Complexity is 
+# O(N1+N2)
+
 def findUnion(arr1,arr2):
-    result=set()
+    result=[]
     i=0
     j=0
     n1=len(arr1)
     n2=len(arr2)
     
+    arr1.sort()
+    arr2.sort()
+    
     while i<n1 and j<n2:
         if arr1[i]<=arr2[j]:
-            if len(result)==0 or arr1[i] not in result:
-                result.add(arr1[i])
+            if len(result)==0 or arr1[i] !=result[-1]:
+                result.append(arr1[i])
             i+=1
         else:
-            if len(result)==0 or arr2[j] not in result :
-                result.add(arr2[j])
+            if len(result)==0 or arr2[j] !=result[-1] :
+                result.append(arr2[j])
             j+=1
     while j<n2:
-        if len(result)==0 or arr2[j] not in result:
-            result.add(arr2[j])
+        if len(result)==0 or arr2[j] != result[-1]:
+            result.append(arr2[j])
         j+=1
     while i<n1:
-        if len(result)==0 or arr1[i] not in result:
-            result.add(arr1[i])
+        if len(result)==0 or arr1[i] !=result[-1]:
+            result.append(arr1[i])
         i+=1
     return len(result)
 
