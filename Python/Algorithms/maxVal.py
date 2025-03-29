ceaaf0bd6c0b7f33d5917ee56db9eb10c833aebb
Arjun Devendra <arjundevendra34@gmail.com>
2025-03-29 19:25:08 +0530

    Maximum number in sorted rotated array

---
+++ Python/Algorithms/maxVal.py
@@ -0,0 +1,28 @@
+import sys
+
+def maxVal(arr,low,high):
+
+    maxVals=-sys.maxsize-1
+    index=0
+
+    while low<=high:
+
+        mid=low+(high-low)//2
+
+        if arr[low]<=arr[mid]:
+            maxVals=max(maxVals,arr[mid])
+            low=mid+1
+        else:
+            maxVals=max(maxVals,arr[high])
+            high=mid-1
+    index=arr.index(maxVals)
+    if index==len(arr)-1:
+        return 0
+    else:
+        return index+1
+
+
+
+
+arr=[6, 9, 2, 4]
+print(maxVal(arr,0,len(arr)-1))
