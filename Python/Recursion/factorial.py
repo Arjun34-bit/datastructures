72fd198d060b214ac1ae1202168b0cb04f04e7cb
Arjun Devendra <arjundevendra34@gmail.com>
2025-04-03 21:10:43 +0530

    factorial

---
+++ Python/Recursion/factorial.py
@@ -0,0 +1,14 @@
+def factorial(sums,n):
+    if n==0:
+        return sums
+    sums=sums*n
+    n-=1
+    val=factorial(sums,n)
+    return val
+    
+def main(n):
+    sums=1
+    result=factorial(sums,n)
+    return result
+    
+print(main(5))
