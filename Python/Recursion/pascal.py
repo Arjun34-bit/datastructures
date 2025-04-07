f7a4882ef49435eb347244eeb9df3ee96f57eae0
Arjun Devendra <arjundevendra34@gmail.com>
2025-04-07 21:11:11 +0530

    pascal traingle

---
+++ Python/Recursion/pascal.py
@@ -0,0 +1,22 @@
+class Solution:
+	def pascal(self,result,row,col,ans):
+		if col==(row):
+			return result
+		ans=ans*(row-col)
+		ans=ans//col
+		result.append(ans)
+		self.pascal(result,row,col+1,ans)
+		return result
+
+	def nthRowOfPascalTriangle(self,n):
+		result=[1]
+		ans=1
+		return self.pascal(result,n,1,ans)
+	
+
+
+
+result=[1]
+ans=1
+obj=Solution()
+print(obj.nthRowOfPascalTriangle(5))
