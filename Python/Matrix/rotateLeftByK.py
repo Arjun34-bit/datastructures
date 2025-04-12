a1cf40389ad6af02500ac6ef2a6b25f83d4efa6c
Arjun Devendra <arjundevendra34@gmail.com>
2025-04-12 21:26:18 +0530

    rotate matrix left by k

---
+++ Python/Matrix/rotateLeftByK.py
@@ -0,0 +1,47 @@
+def reverse(arr,k):
+    n=len(arr)
+        
+    l=0
+    r=n-1
+    
+    while l<r:
+        arr[l],arr[r]=arr[r],arr[l]
+        l+=1
+        r-=1
+        
+    l=0
+    right=n-k
+    while l<right:
+        arr[l],arr[right]=arr[right],arr[l]
+        l+=1
+        right-=1
+        
+    l=n-k+1
+    r=n-1
+    while l<r:
+        arr[l],arr[r]=arr[r],arr[l]
+        l+=1
+        r-=1
+        
+    return arr
+
+def rotateByK(mat,k):
+    row=len(mat)
+    col=len(mat[0])
+    
+    
+    low=0
+    right=row-1
+    
+    k=k%col
+    
+    while low<=right:
+        reverse(mat[low],k+1)
+        low+=1
+    return mat
+    
+    
+    
+    
+mat=[[1,2,3],[4,5,6],[7,8,9]]
+print(rotateByK(mat,1))
