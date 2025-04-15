2ec056026895c3015690b7fb729026fd4f7d18aa
Arjun Devendra <arjundevendra34@gmail.com>
2025-04-15 16:26:51 +0530

    findPeak changed

--- Python/Algorithms/maxNumber.py
+++ Python/Algorithms/maxNumber.py
@@ -4,24 +4,20 @@ def maxVal(arr,low,high):
 
     maxVals=-sys.maxsize-1
 
-    idx=0
-
     while low<=high:
 
         mid=low+(high-low)//2
 
         if arr[low]<=arr[mid]:
             maxVals=max(maxVals,arr[mid])
-            idx=mid
             low=mid+1
         else:
             maxVals=max(maxVals,arr[high])
-            idx=high
             high=mid-1
-    return [idx,maxVals]
+    return maxVals
 
 
 
 
-arr=[6, 9, 2, 4]
+arr=[10,50,40,30,20]
 print(maxVal(arr,0,len(arr)-1))
