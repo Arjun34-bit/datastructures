e2e7b883a70ed92e24297624bd0aaf11a5d6cd27
Arjun Devendra <arjundevendra34@gmail.com>
2025-04-15 14:57:17 +0530

    comments removed

--- Python/Matrix/setMatrix0.py
+++ Python/Matrix/setMatrix0.py
@@ -30,30 +30,25 @@ def setZeroes(matrix):
     row = len(matrix)
     col = len(matrix[0])
 
-    col0 = 1  # To check separately if first column needs to be zero
-
-    # Step 1: Mark rows and columns
+    col0 = 1  
     for i in range(row):
         for j in range(col):
             if matrix[i][j] == 0:
-                matrix[i][0] = 0  # Mark the row
+                matrix[i][0] = 0  
                 if j != 0:
-                    matrix[0][j] = 0  # Mark the column
+                    matrix[0][j] = 0  
                 else:
-                    col0 = 0  # First column will need to be 0
+                    col0 = 0  
 
-    # Step 2: Set matrix[i][j] = 0 if either its row or column was marked
     for i in range(1, row):
         for j in range(1, col):
             if matrix[i][0] == 0 or matrix[0][j] == 0:
                 matrix[i][j] = 0
 
-    # Step 3: Handle the first row
     if matrix[0][0] == 0:
         for j in range(col):
             matrix[0][j] = 0
 
-    # Step 4: Handle the first column
     if col0 == 0:
         for i in range(row):
             matrix[i][0] = 0
