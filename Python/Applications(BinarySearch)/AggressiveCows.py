c522f7674d312ffb118d5adf54f79c2632890745
Arjun Devendra <arjundevendra34@gmail.com>
2025-04-18 21:55:50 +0530

    aggressive cows binary appraoch

--- Python/Applications(BinarySearch)/AggressiveCows.py
+++ Python/Applications(BinarySearch)/AggressiveCows.py
@@ -30,7 +30,41 @@ class LinearApproach():
 
 
 
+class BinaryAppraoch():
+    def canWePlaceCows(self,arr,dis,cows):
+        cowCount=1
+        last=arr[0]
+
+        for i in range(1,len(arr)):
+            if(arr[i]-last >= dis):
+                cowCount+=1
+                last=arr[i]
+
+            if(cowCount >= cows):
+                return True
+
+
+    def aggressiveCows(self,arr,k):
+        low=1
+        high=arr[-1]-arr[0]
+        result=0
+
+        while low<=high:
+            mid=low+(high-low)//2
+
+            if (self.canWePlaceCows(arr,mid,k)):
+                low=mid+1
+            else:
+                high=mid-1
+        return high
+            
+
+
+
 obj=LinearApproach()
+obj1=BinaryAppraoch()
+
 stalls=[1,2,4,8,9]
 cows=3
-print(obj.aggressiveCows(stalls,cows))
+print("Linear Approach ",obj.aggressiveCows(stalls,cows))
+print("Binary Approach ",obj1.aggressiveCows(stalls,cows))
