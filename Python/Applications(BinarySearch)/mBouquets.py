0f7ff82abea8e979c2488ded3e98b0af8bd3d690
Arjun Devendra <arjundevendra34@gmail.com>
2025-04-21 21:37:25 +0530

    minimum no of days for m bouquets binary search

--- Python/Applications(BinarySearch)/mBouquets.py
+++ Python/Applications(BinarySearch)/mBouquets.py
@@ -30,53 +30,53 @@ class LinearSearch:
         return -1
 
 
-# class BinarySearch:
-#     def calculateMinDays(self,bloomDays,bouquets,flowers,day):
-#         n=len(bloomDays)
-#         adjFlowers=0
-#         nBouquets=0
+class BinarySearch:
+    def calculateMinDays(self,bloomDays,bouquets,flowers,day):
+        n=len(bloomDays)
+        adjFlowers=0
+        nBouquets=0
         
-#         for blooms in range(0,n):
-#             # print(nBouquets)
-#             if bloomDays[blooms]<=day:
-#                 adjFlowers+=1
-#             else:
-#                 nBouquets+=adjFlowers//flowers
-#                 adjFlowers=0
+        for blooms in range(0,n):
+            # print(nBouquets)
+            if bloomDays[blooms]<=day:
+                adjFlowers+=1
+            else:
+                nBouquets+=adjFlowers//flowers
+                adjFlowers=0
             
-#         nBouquets+=adjFlowers//flowers    
-#         if(nBouquets>=bouquets):
-#             return True
-#         else:
-#             return False
+        nBouquets+=adjFlowers//flowers    
+        if(nBouquets>=bouquets):
+            return True
+        else:
+            return False
         
-#     def bloom(self,bloomDay,bouquets,flowers):
-#         low=min(bloomDay)
-#         high=max(bloomDay)
+    def bloom(self,bloomDay,bouquets,flowers):
+        low=min(bloomDay)
+        high=max(bloomDay)
 
-#         ans=high
+        ans=high
                 
         
-#         while low<=high:
-#             mid=low+(high-low)//2
+        while low<=high:
+            mid=low+(high-low)//2
             
-#             if(self.calculateMinDays(bloomDay,bouquets,flowers,mid)):
-#                 ans=mid
-#                 high=mid-1
-#             else:
-#                 low=mid+1
+            if(self.calculateMinDays(bloomDay,bouquets,flowers,mid)):
+                ans=mid
+                high=mid-1
+            else:
+                low=mid+1
                 
-#         return ans
+        return ans
 
 
 obj=LinearSearch()  #Time Complexity is O(min - max + 1) * O(n)  for worst case
 
-# obj1=BinarySearch()  #Time Complexity is O(log2(min - max + 1)) * O(n)  for worst case
+obj1=BinarySearch()  #Time Complexity is O(log2(min - max + 1)) * O(n)  for worst case
 
 bloomD=[1,10,3,10,2]
 m=3
 k=1
 print("Linear Search",obj.bloom(bloomD,m,k))
-# print("Binary Search",obj1.bloom(bloomD,m,k))
+print("Binary Search",obj1.bloom(bloomD,m,k))
         
         
