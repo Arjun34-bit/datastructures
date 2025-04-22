bd30a2c0dacaa3064b44ecbdf58de04e0cd6c98a
Arjun Devendra <arjundevendra34@gmail.com>
2025-04-22 21:44:25 +0530

    least capacity to ship n weights using binary search

--- Python/Applications(BinarySearch)/leastCapacityDdays.py
+++ Python/Applications(BinarySearch)/leastCapacityDdays.py
@@ -26,7 +26,43 @@ class LinearSearch:
     
 
 
+class BinarySearch:
+    def calculateLeast(self,prod,days,cap):
+        currCap=0
+        currDay=1
+
+        for day in range(0,len(prod)):
+            if currCap+prod[day]<=cap:
+                currCap+=prod[day]
+            else:
+                currCap=prod[day]
+                currDay+=1
+        return currDay
+
+    def weights(self,prod,d):
+        low=max(prod)
+        high=sum(prod)
+
+        while low<=high:
+            mid=low+(high-low)//2
+            
+            reqDays=self.calculateLeast(prod,d,mid)
+            
+            if reqDays<=d:
+                high=mid-1
+            else:
+                low=mid+1
+                
+            
+        return low
+    
+
+
+
+
 obj=LinearSearch()
+obj1=BinarySearch()
 arr=[1,2,1]
 d=2
-print(obj.weights(arr,d))
+print("Linear Search ",obj.weights(arr,d))
+print("Binary Search ",obj1.weights(arr,d))
