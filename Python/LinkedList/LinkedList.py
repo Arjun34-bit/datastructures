cdd8aed9667c07e36e8efe613b18766642eb5965
Arjun Devendra <arjundevendra34@gmail.com>
2025-04-22 14:41:22 +0530

    searching, traversing and length of linked list

--- Python/LinkedList/LinkedList.py
+++ Python/LinkedList/LinkedList.py
@@ -13,8 +13,42 @@ def convertToLL(arr):
         mover=temp
         
     return head
+
+
+
+def searchInLL(head,target):
+    mover=head
+
+    while mover:
+        
+        if(mover.data == target):
+            return True
+        else:
+            mover=mover.next
+            
+    return False
     
+    
+def traverseInLL(head):
+    mover=head
+    while mover:
+        print(mover.data,end="->")
+        mover=mover.next
+        
+        
+def lengthOfLL(head):
+    mover=head
+    count=0
+    while mover:
+        count+=1
+        mover=mover.next
+        
+    return count
         
 arr=[15,2,3,4,5,6]
 headVal=convertToLL(arr)
-print(headVal)
+print("Searching for target element in Linked List ",searchInLL(headVal,6))
+print("Traversing Linked List",end=" ")
+print(traverseInLL(headVal))
+print("Length of Linked List is ",lengthOfLL(headVal))
+# print(headVal)
