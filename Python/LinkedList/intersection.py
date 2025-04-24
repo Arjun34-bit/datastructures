e5e002f34ca9ec3fc53d07b33b7a1746f5c6db9e
Arjun Devendra <arjundevendra34@gmail.com>
2025-04-24 14:29:19 +0530

    intersection partially done

---
+++ Python/LinkedList/intersection.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+        
+        
+def convertToLL(arr):
+    head=Node(arr[0])
+    mover=head
+    
+    for i in range(1,len(arr)):
+        temp=Node(arr[i])
+        mover.next=temp
+        mover=temp
+        
+    return head
+    
+    
+ll1=[4,4,4,4,4]
+ll2=[4,4,4]
+head1=convertToLL(ll1)
+head2=convertToLL(ll2)
+
+
+def intersection(head1,head2):
+    mp={}
+    
+    temp=head1
+    
+    while temp:
+        mp[temp]=True
+        temp=temp.next
+        
+        
+    temp=head2
+    while temp != None:
+        if mp[temp] in mp:
+            return temp.data
+        temp=temp.next
+        
+    return -1
+        
+        
+print(intersection(head1,head2))
