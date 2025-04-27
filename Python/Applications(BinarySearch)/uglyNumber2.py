aed655ff6de354bf65b946a93c5ab91a99c0bdb9
Arjun Devendra <arjundevendra34@gmail.com>
2025-04-27 20:06:41 +0530

    ugly number 2

---
+++ Python/Applications(BinarySearch)/uglyNumber2.py
@@ -0,0 +1,26 @@
+def uglyNumber(n):
+    dp=[0]*n
+    dp[0]=1
+    p1=0
+    p2=0
+    p3=0
+
+    for i in range(1,n):
+        twoM = dp[p1] * 2
+        threeM = dp[p2] * 3
+        fiveM = dp[p3] * 5
+
+        dp[i]=min(twoM,min(threeM,fiveM))
+        
+        if dp[i] == twoM:
+            p1+=1
+
+        if dp[i] == threeM:
+            p2+=1
+
+        if dp[i] == fiveM:
+            p3+=1
+
+    return dp[n-1]
+    
+print(uglyNumber(10))
