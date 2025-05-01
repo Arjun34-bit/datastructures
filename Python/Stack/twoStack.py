4f19731074740dfed4f7083071c631bbe31adbc0
Arjun Devendra <arjundevendra34@gmail.com>
2025-05-01 20:58:40 +0530

    two stack in an array

---
+++ Python/Stack/twoStack.py
@@ -0,0 +1,35 @@
+class TwoStacks:
+    def __init__(self):
+        self.arr1=[]
+        self.arr2=[]
+
+    def push1(self, x):
+        self.arr1.append(x)
+        pass
+
+    def push2(self, x):
+        self.arr2.append(x)
+        pass
+
+    def pop1(self):
+        if len(self.arr1)==0:
+            return -1
+        poped=self.arr1.pop()
+        return poped
+
+    def pop2(self):
+        if len(self.arr2)==0:
+            return -1
+        poped=self.arr2.pop()
+        return poped
+    
+
+
+s=TwoStacks()
+s.push1(10)
+s.push1(20)
+s.push2(30)
+s.push2(40)
+
+print(s.pop1())
+print(s.pop2())
