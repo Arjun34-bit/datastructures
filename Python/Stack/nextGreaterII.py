4f7ab8a7b368abc15b236ecf6196782999e8f0df
Arjun Devendra <arjundevendra34@gmail.com>
2025-05-02 21:16:48 +0530

    next greater element problem 2

---
+++ Python/Stack/nextGreaterII.py
@@ -0,0 +1,27 @@
+def nextGreaterElements(nums):
+    n=len(nums)
+
+    if n<=1:
+        return [-1]
+
+    nge=[-1] * n
+    st=[]
+
+    for i in range(2*n-1,-1,-1):
+
+        while st and st[-1] <= nums[i%n]:
+            st.pop()
+
+        if i<2*n:
+            if st:
+                nge[i%n]=st[-1]
+            else:
+                nge[i%n]=-1
+
+        st.append(nums[i%n])
+
+    return nge
+        
+        
+arr=[1,2,1]
+print(nextGreaterElements(arr))        #  TC:O(2n + 2n)  ~ TC:O(n)  and SC:O(n)
