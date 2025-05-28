f4d00a75c2d17f1b915c75caf29babbf65b4ee4b
Arjun Devendra <arjundevendra34@gmail.com>
2025-05-28 20:27:11 +0530

    queue collection

--- Python/Queue/Queue.py
+++ Python/Queue/Queue.py
@@ -15,3 +15,31 @@ class MyQueue:
         popped=self.q.pop(0)
         return popped
          # add code here
+
+
+#From Queue Collection
+from queue import Queue
+
+
+def push(x):
+
+    global queue_1
+    global queue_2
+    # code here
+    
+    queue_2.put(x)
+    
+    while not queue_1.empty():
+        queue_2.put(queue_1.get())
+        
+    queue_1,queue_2=queue_2,queue_1
+
+
+def pop():
+
+    global queue_1
+    global queue_2
+    
+    if not queue_1.empty():
+        return queue_1.get()
+    return -1
