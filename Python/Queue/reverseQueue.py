024c4b492c70cae65edef1fa05aafbebed11788a
Arjun Devendra <arjundevendra34@gmail.com>
2025-06-02 19:21:21 +0530

    reverse queue using stack

--- Python/Queue/reverseQueue.py
+++ Python/Queue/reverseQueue.py
@@ -1,3 +1,32 @@
+from queue import Queue
+
+def reverseQueue(q):      #Brute Force Approach
+    st=[]
+    while q.qsize() != 0:
+        st.append(q.get())
+        
+    print(st)
+        
+        
+    while st:
+        q.put(st.pop())
+        
+        
+    return q
+    
+    
+    
+q=Queue()
+q.put(1)
+q.put(2)
+q.put(3)
+
+print(reverseQueue(q))
+        
+       
+            
+        
+
 def reverseQueue( q):
     if q.empty():
         return q
